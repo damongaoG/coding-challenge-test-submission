9c30d2fd87dcab9b614c150bd9c3e4deacf32602
damongaoG <gaochujian159@gmail.com>
2025-08-11 22:44:37 +1000

    feat: make application responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
